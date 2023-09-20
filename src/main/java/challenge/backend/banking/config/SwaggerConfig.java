df168da81924eddc86fdebba2e67683699b3fec6
Ralf Drehmer Wink <ralf_wink@gitlab.sicredi.net>
2023-09-20 13:57:14 -0300

    adicionando swagger config

---
+++ src/main/java/challenge/backend/banking/config/SwaggerConfig.java
@@ -0,0 +1,23 @@
+package challenge.backend.banking.config;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Contact;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.info.License;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SwaggerConfig {
+
+    @Bean
+    public OpenAPI apiInfo() {
+        return new OpenAPI().info(new Info().title("Challenge Backend Banking")
+                .description("")
+                .contact(new Contact().name("Ralf Drehmer Wink")
+                        .url("http://github.com/ralfdw3"))
+                .version("1.0.0")
+                .license(new License())
+                .termsOfService(""));
+    }
+}
