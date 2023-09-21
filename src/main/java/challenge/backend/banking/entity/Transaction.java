4d44a1f5c38c0de0ef5c53c12b074cdbe378477a
Ralf Drehmer Wink <ralf_wink@gitlab.sicredi.net>
2023-09-21 10:54:38 -0300

    adicionando relacoes do banco na transaction

--- src/main/java/challenge/backend/banking/entity/Transaction.java
+++ src/main/java/challenge/backend/banking/entity/Transaction.java
@@ -1,10 +1,8 @@
-package challenge.backend.banking.entity.transaction;
+package challenge.backend.banking.entity;
 
 import challenge.backend.banking.entity.User;
-import jakarta.persistence.Entity;
-import jakarta.persistence.Table;
+import jakarta.persistence.*;
 import lombok.Builder;
-import org.springframework.data.annotation.Id;
 
 import java.math.BigDecimal;
 
@@ -13,8 +11,13 @@ import java.math.BigDecimal;
 @Builder
 public class Transaction {
     @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     Long id;
+    @ManyToOne
+    @JoinColumn(name = "receiver_id")
     User receiver;
+    @ManyToOne
+    @JoinColumn(name = "payer_id")
     User payer;
     BigDecimal value;
 }
