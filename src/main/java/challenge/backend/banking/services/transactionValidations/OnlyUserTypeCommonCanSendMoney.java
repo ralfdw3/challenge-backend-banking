9726df3d0cf284ce07386643ea3074e5c4b6a4a8
Ralf Drehmer Wink <ralf_wink@gitlab.sicredi.net>
2023-09-21 14:14:03 -0300

    Adicionando logs nas verificacoes

--- src/main/java/challenge/backend/banking/services/transactionValidations/OnlyUserTypeCommonCanSendMoney.java
+++ src/main/java/challenge/backend/banking/services/transactionValidations/OnlyUserTypeCommonCanSendMoney.java
@@ -4,13 +4,22 @@ import challenge.backend.banking.entities.User;
 import challenge.backend.banking.entities.inputs.TransactionInput;
 import challenge.backend.banking.enums.UserType;
 import challenge.backend.banking.exceptions.BadRequestException;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Component;
 
 @Component
+@Slf4j
 public class OnlyUserTypeCommonCanSendMoney implements TransactionValidations {
     @Override
     public void validate(User payer, TransactionInput transactionInput) {
-        if (payer.getUserType() == UserType.MERCHANT){
+        UserType PayerUserType = payer.getUserType();
+
+        if (PayerUserType == UserType.MERCHANT){
+            log.error("""
+                    Erro na regra: OnlyUserTypeCommonCanSendMoney.
+                    Tipo do pagador: {}
+                    """, PayerUserType);
+
             throw new BadRequestException("Lojistas não podem realizar transações");
         }
     }
--- src/main/java/challenge/backend/banking/services/transactionValidations/PayerHaveEnoughMoneyToSend.java
+++ src/main/java/challenge/backend/banking/services/transactionValidations/PayerHaveEnoughMoneyToSend.java
@@ -3,18 +3,31 @@ package challenge.backend.banking.services.transactionValidations;
 import challenge.backend.banking.entities.User;
 import challenge.backend.banking.entities.inputs.TransactionInput;
 import challenge.backend.banking.exceptions.BadRequestException;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Component;
 
+import java.math.BigDecimal;
+
 @Component
+@Slf4j
 public class PayerHaveEnoughMoneyToSend implements TransactionValidations {
     @Override
     public void validate(User payer, TransactionInput transactionInput) {
-        if (payerDoesntHaveEnoughMoney(payer, transactionInput)){
+        BigDecimal payerBalance = payer.getBalance();
+        BigDecimal transactionAmount = transactionInput.amount();
+
+        if (payerDoesntHaveEnoughMoney(payerBalance, transactionAmount)){
+            log.error("""
+                    Erro na regra: PayerHaveEnoughMoneyToSend.
+                    Saldo do pagador: {}
+                    Quantia a ser paga: {}
+                    """, payerBalance, transactionAmount);
+
             throw new BadRequestException("Saldo insuficiente para realizar a transação");
         }
     }
 
-    private static Boolean payerDoesntHaveEnoughMoney(User payer, TransactionInput transactionInput) {
-        return payer.getBalance().compareTo(transactionInput.amount()) < 0;
+    private static Boolean payerDoesntHaveEnoughMoney(BigDecimal payerBalance, BigDecimal transactionAmount) {
+        return payerBalance.compareTo(transactionAmount) < 0;
     }
 }
