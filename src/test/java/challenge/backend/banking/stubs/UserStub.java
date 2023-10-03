02b1bad5af23bbbd35ce1f90061000a5a352b549
Ralf Drehmer Wink <ralf_wink@gitlab.sicredi.net>
2023-10-03 09:14:38 -0300

    Reutilizando stubs para facilitar manutencao

--- src/test/java/challenge/backend/banking/stubs/UserStub.java
+++ src/test/java/challenge/backend/banking/stubs/UserStub.java
@@ -15,38 +15,33 @@ public interface UserStub {
     String EMAIL_RECEIVER = "receiver@email.com.br";
     String DOCUMENT_ALL_ZERO = "000.000.000-00";
     BigDecimal BALANCE_HUNDRED = valueOf(100);
+    BigDecimal BALANCE_ZERO = BigDecimal.ZERO;
     UserType USER_TYPE_COMMON = UserType.COMMON;
     UserType USER_TYPE_MERCHANT = UserType.MERCHANT;
 
-    static User commonUser() {
+    static User.UserBuilder userBuilderDefault() {
         return User.builder()
                 .name(NAME)
                 .password(PASSWORD)
                 .email(EMAIL_PAYER)
                 .document(DOCUMENT_ALL_ZERO)
                 .balance(BALANCE_HUNDRED)
-                .userType(USER_TYPE_COMMON)
-                .build();
+                .userType(USER_TYPE_COMMON);
+    }
+
+    static User commonUser() {
+        return userBuilderDefault().build();
     }
 
     static User commonUserWithNoMoney() {
-        return User.builder()
-                .name(NAME)
-                .password(PASSWORD)
-                .email(EMAIL_PAYER)
-                .document(DOCUMENT_ALL_ZERO)
-                .balance(BigDecimal.ZERO)
-                .userType(USER_TYPE_COMMON)
+        return userBuilderDefault()
+                .balance(BALANCE_ZERO)
                 .build();
     }
 
     static User merchantUser() {
-        return User.builder()
-                .name(NAME)
-                .password(PASSWORD)
+        return userBuilderDefault()
                 .email(EMAIL_RECEIVER)
-                .document(DOCUMENT_ALL_ZERO)
-                .balance(BALANCE_HUNDRED)
                 .userType(USER_TYPE_MERCHANT)
                 .build();
     }
